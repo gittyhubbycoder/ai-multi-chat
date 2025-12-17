14865a0be5117eefd31f67443412f423a87437c3
djanbjar <diyan.bahar@outlook.com>
2025-12-17 19:12:01 +0000

    Single File Update

--- src/utils/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './constants';
-
-export const supabase = SUPABASE_URL && SUPABASE_ANON_KEY 
-  ? createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-  : null;
